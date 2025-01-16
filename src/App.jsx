7cda96b962ed34b0a331f29c01dca17d5f656bc9
Mahipal Dhanani <dhananimahipal144@gmail.com>
2025-01-16 11:22:18 +0530

    React.lazy and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import "./App.css";
 // import About from "./Components/About";
 // import Home from "./Components/Home";
 
-const Home = lazy(() => import("./Components/Home.jsx"));
-const About = lazy(() => import("./Components/About.jsx"));
+// const Home = lazy(() => import("./Components/Home.jsx"));
+// const About = lazy(() => import("./Components/About.jsx"));
 function App() {
   return (
     <>
-      <div className="p-4 flex justify-center items-center  flex-col">
+    {/* ............React.lazy and Suspense.............. */}
+      {/* <div className="p-4 flex justify-center items-center  flex-col">
         <h1>React.lazy and Suspense</h1>
         <Suspense fallback={<h1 className="text-red-500">Home component is Loading......</h1>}>
           <Home />
@@ -16,7 +17,12 @@ function App() {
         <Suspense fallback={<h1 className="text-red-500">About component is Loading......</h1>}>
           <About />
         </Suspense>
-      </div>
+      </div> */}
+
+
+
+
+
     </>
   );
 }
