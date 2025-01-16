5b8094775a1a6b38afd8dcf3765687bbc8b95ccd
Mahipal Dhanani <dhananimahipal144@gmail.com>
2025-01-16 10:38:09 +0530

    Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="p-4 flex justify-center items-center  flex-col">
+        <h1>React.lazy and Suspense</h1>  
+      </div>
+    </>
+  );
+}
+
+export default App;
